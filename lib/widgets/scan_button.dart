889934e7ba370b70066c2c04729d046673f5ba74
rojo5 <luis21197@gmail.com>
2022-01-16 12:21:03 +0100

    Añadida libreria para escanear QR

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 class ScanButton extends StatelessWidget {
   const ScanButton({Key? key}) : super(key: key);
 
@@ -7,7 +8,12 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       elevation: 0,
       child: Icon(Icons.qr_code_scanner),
-      onPressed: () {},
+      onPressed: () async{
+
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
+
+        print(barcodeScanRes);
+      },
     );
   }
 }
